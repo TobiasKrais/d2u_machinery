c3a166d7d12824ae8320d579189d02d5cc2e1272
Tobias Krais <tk@design-to-use.de>
2016-12-21 21:53:49 +0100

    version 1.0.1

--- update.php
+++ update.php
@@ -3,5 +3,5 @@
 d2u_machinery_lang_helper::factory()->install();
 
 // Update modules
-$d2u_module_manager = new D2UModuleManager(D2UMachineryModules::getD2UMachineryModules());
+$d2u_module_manager = new D2UModuleManager(D2UMachineryModules::getD2UMachineryModules(), "", "d2u_machinery");
 $d2u_module_manager->autoupdate();
