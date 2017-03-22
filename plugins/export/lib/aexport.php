2a4cb60e20fd25ef5c566c4ed7d1c0e2a7fde130
Tobias Krais <tk@design-to-use.de>
2017-03-22 22:51:38 +0100

    Fix https://github.com/redaxo/redaxo/issues/707#issuecomment-280859011

--- plugins/export/lib/aexport.php
+++ plugins/export/lib/aexport.php
@@ -54,22 +54,7 @@ abstract class AExport {
 	 * @return string Cached picture filename
 	 */
 	protected function preparePicture($pic) {
-		$media = new rex_managed_media(rex_path::media($pic));
-		$media_manager = new rex_media_manager($media);
-		$media_manager->setCachePath(rex_path::addonCache("media_manager"));
-		try {
-			// ApplyEffects is only protected!!
-			$media_manager->applyEffects($this->provider->media_manager_type);
-			if($media_manager->isCached() == FALSE) {
-				ob_start();
-				$media->sendMedia($media_manager->getCacheFilename(), $media_manager->getHeaderCacheFilename(), TRUE);
-				ob_end_clean();
-			}
-		}
-		catch (Error $e) {
-			getimagesize(rex::getServer()."index.php?rex_media_type=". $this->provider->media_manager_type ."&rex_media_file=". $pic);
-		}
-
+		$media_manager = rex_media_manager::create($this->provider->media_manager_type, $pic);
 		if(file_exists($media_manager->getCacheFilename())) {
 			// Cached file if successfull
 			return $media_manager->getCacheFilename();
