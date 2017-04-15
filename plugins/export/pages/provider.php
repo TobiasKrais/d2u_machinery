1c277a9251403a2462815af2609e6ccfc7c884c2
Tobias Krais <tk@design-to-use.de>
2017-04-15 22:43:58 +0200

    Simplification calling needed CSS

--- plugins/export/pages/provider.php
+++ plugins/export/pages/provider.php
@@ -157,7 +157,7 @@ if ($func == 'edit' || $func == 'add') {
 	</form>
 	<br>
 	<?php
-		print d2u_addon_backend_helper::getCSS('d2u_machinery');
+		print d2u_addon_backend_helper::getCSS();
 		print d2u_addon_backend_helper::getJS();
 }
 
