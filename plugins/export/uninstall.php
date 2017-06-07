540971c01e34b8fa34605b06d9c5f3cd7e2a722b
Tobias Krais <tk@design-to-use.de>
2017-06-07 22:37:43 +0200

    uninstall everything

--- plugins/export/uninstall.php
+++ plugins/export/uninstall.php
@@ -2,6 +2,11 @@
 $sql = rex_sql::factory();
 
 // Delete tables
+// Delete tables
+$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'd2u_machinery_export_machines');
+$sql->setQuery('DROP TABLE IF EXISTS ' . rex::getTablePrefix() . 'd2u_machinery_export_properties');
+
+// Delete category extensions
 $sql->setQuery('ALTER TABLE ' . rex::getTablePrefix() . 'd2u_machinery_categories
 	DROP export_machinerypark_category_id,
 	DROP export_europemachinery_category_id,
