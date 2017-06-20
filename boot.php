83f35fa510945d4c10003d04663655632aa966e7
Tobias Krais <tk@design-to-use.de>
2017-06-20 14:40:08 +0200

    move settings to role options

--- boot.php
+++ boot.php
@@ -3,7 +3,7 @@ if(rex::isBackend() && is_object(rex::getUser())) {
 	rex_perm::register('d2u_machinery[]', rex_i18n::msg('d2u_machinery_rights_all'));
 	rex_perm::register('d2u_machinery[edit_lang]', rex_i18n::msg('d2u_machinery_rights_edit_lang'), rex_perm::OPTIONS);
 	rex_perm::register('d2u_machinery[edit_tech_data]', rex_i18n::msg('d2u_machinery_rights_edit_tech_data'), rex_perm::OPTIONS);
-	rex_perm::register('d2u_machinery[settings]', rex_i18n::msg('d2u_machinery_rights_settings'));	
+	rex_perm::register('d2u_machinery[settings]', rex_i18n::msg('d2u_machinery_rights_settings'), rex_perm::OPTIONS);	
 }
 
 if(rex::isBackend()) {
