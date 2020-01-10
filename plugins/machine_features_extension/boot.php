cd04d946bad451d10457b774550a685315688bf6
Tobias Krais <tk@design-to-use.de>
2020-01-10 13:06:24 +0100

    remove feature title

--- plugins/machine_features_extension/boot.php
+++ plugins/machine_features_extension/boot.php
@@ -38,7 +38,7 @@ function rex_d2u_machinery_features_media_is_in_use(rex_extension_point $ep) {
 
 	// Features
 	$sql = \rex_sql::factory();
-	$sql->setQuery('SELECT lang.feature_id, title FROM `' . \rex::getTablePrefix() . 'd2u_machinery_features_lang` AS lang '
+	$sql->setQuery('SELECT lang.feature_id, name FROM `' . \rex::getTablePrefix() . 'd2u_machinery_features_lang` AS lang '
 		.'LEFT JOIN `' . \rex::getTablePrefix() . 'd2u_machinery_features` AS features ON lang.feature_id = features.feature_id '
 		.'WHERE pic = "'. $filename .'" AND clang_id = '. rex_config::get("d2u_helper", "default_lang"));
 	
@@ -46,7 +46,7 @@ function rex_d2u_machinery_features_media_is_in_use(rex_extension_point $ep) {
 	// Features
 	for($i = 0; $i < $sql->getRows(); $i++) {
 		$message =  '<a href="javascript:openPage(\'index.php?page=d2u_machinery/machine_features_extension&func=edit&entry_id='.
-			$sql->getValue('feature_id') .'\')">'. rex_i18n::msg('d2u_machinery_rights_all') ." - ". rex_i18n::msg('d2u_machinery_features') .': '. $sql->getValue('title') .'</a>';
+			$sql->getValue('feature_id') .'\')">'. rex_i18n::msg('d2u_machinery_rights_all') ." - ". rex_i18n::msg('d2u_machinery_features') .': '. $sql->getValue('name') .'</a>';
   		if(!in_array($message, $warning)) {
 			$warning[] = $message;
 		}
