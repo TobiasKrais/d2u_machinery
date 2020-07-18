d1824b87aab1e7178ebc82dd9a91aad3b85db77c
Tobias Krais <tk@design-to-use.de>
2020-07-18 08:58:44 +0200

    prepare 1.3.4

--- pages/setup.php
+++ pages/setup.php
@@ -46,6 +46,10 @@ $d2u_module_manager->showManagerList();
 	</form>
 </fieldset>
 <h2>Changelog</h2>
+<p>1.3.4-DEV:</p>
+<ul>
+	<li>...</li>
+</ul>
 <p>1.3.3:</p>
 <ul>
 	<li>Benötigt Redaxo >= 5.10, da die neue Klasse rex_version verwendet wird.</li>
